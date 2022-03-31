476d9922e73404d8780e148883ca81f96eb91df1
defstiller <81063003+defstiller@users.noreply.github.com>
2022-03-30 20:45:46 -0500

    updated reducer to use Switch

--- src/reducer.js
+++ src/reducer.js
@@ -1,29 +1,31 @@
 function reducer (state, action) {
-    if (action.type === "ADD_ITEM") {
-        const newGrocery = [...state.groceries, action.payload]
-        return {
+    switch (action.type) {
+        case "ADD_ITEM":
+            const newGrocery = [...state.groceries, action.payload]
+            return {
             groceries: newGrocery,
             isModalOpen: true,
             modalContent: 'Success'
-        }
-    } else if (action.type === "NO_NAME") {
-        return {...state, isModalOpen: true, modalContent: 'Please enter a name'}
-    } else if (action.type === "NO_COMMENT"){
-        return {...state, isModalOpen: true, modalContent: 'Please enter a comment'}
-    } else if (action.type === "CLOSE_MODAL") {
-        return {...state, isModalOpen:false}
-    } else if (action.type === "CLEAR") {
-        return {    
-            groceries: [],
-            isModalOpen: true,
-            modalContent: 'Cleared'}
-    } else if (action.type === "DELETE") {
-        const newGroceries = state.groceries.filter(grocery => grocery.id !== action.payload && grocery)
-        return {
-            groceries:newGroceries,
-            isModalOpen:true,
-            modalContent: 'Deleted'
-        }
+            }
+        case "NO_NAME":
+            return {...state, isModalOpen: true, modalContent: 'Please enter a name'}
+        case "NO_COMMENT":
+            return {...state, isModalOpen: true, modalContent: 'Please enter a comment'}
+        case "CLOSE_MODAL":
+            return {...state, isModalOpen:false}
+        case "CLEAR":
+            return {    
+                groceries: [],
+                isModalOpen: true,
+                modalContent: 'Cleared'}
+        case "DELETE":
+            const newGroceries = state.groceries.filter(grocery => grocery.id !== action.payload && grocery)
+            return {
+                groceries:newGroceries,
+                isModalOpen:true,
+                modalContent: 'Deleted'
+            }
+        default: throw new Error("No matching action type found")
     }
 }
 export default reducer;
